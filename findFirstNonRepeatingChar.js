0d00ecbe43a1070d56502df2634883cce3eb821e
Saim Rizvi <shr2mz@gmail.com>
2022-03-13 23:52:56 -0400

    new algo solved

---
+++ findFirstNonRepeatingChar.js
@@ -0,0 +1,28 @@
+// find first non-repeating character in a string 
+
+const string = 'oh my god dude where is my car'
+const int = '1234123'
+
+// time complexity O(n)
+// space complexity O(n) 
+
+// use hashmap 
+
+function firstNonRepeatingChar(str) {
+    let obj = {}
+
+    for (let elem of str) {
+        obj[elem] = obj[elem] + 1 || 1
+    }
+
+    for (let elem in obj) {
+        if (obj[elem] === 1) {
+            return elem
+        }
+    }
+    return -1
+    
+}
+
+console.log(firstNonRepeatingChar(string))
+console.log(firstNonRepeatingChar(int))
